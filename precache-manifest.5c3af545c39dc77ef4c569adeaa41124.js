04ca60d986f2c8171a9fcee9530cd33bdd6bc8c4
DevanshChoudaha <devanshchoudaha786@gmail.com>
2020-07-25 12:18:25 +0530

    Updates

---
+++ precache-manifest.5c3af545c39dc77ef4c569adeaa41124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "339b0b036e572796d1331233d47ed5db",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "14b95e53ed5d588736c1",
+    "url": "/Portfolio-Website/static/css/main.3ca4ded5.chunk.css"
+  },
+  {
+    "revision": "e44729af9527b5886190",
+    "url": "/Portfolio-Website/static/js/2.2494d31c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio-Website/static/js/2.2494d31c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b95e53ed5d588736c1",
+    "url": "/Portfolio-Website/static/js/main.4c5404ce.chunk.js"
+  },
+  {
+    "revision": "b8c7e0cb4fb17c167de8",
+    "url": "/Portfolio-Website/static/js/runtime-main.5552b3a3.js"
+  }
+]);
